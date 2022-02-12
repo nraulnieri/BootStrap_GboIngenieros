7be96addc091d3709ac6625e3a9f8a8a703049f1
nraulnieri <nraulnieri@gmail.com>
2022-02-12 11:29:05 -0500

    añadir brochure

--- js/index.js
+++ js/index.js
@@ -3,20 +3,25 @@ window.sr = ScrollReveal();
 
 sr.reveal('.cards-index', {
     duration: 3000,
-    interval: 700
+    interval: 500
 });
 
 sr.reveal('.row-superior', {
-    duration: 4000,
-    interval: 700
+    duration: 3000,
+    interval: 500
 });
 
 sr.reveal('.row-principal', {
-    duration: 5000,
-    interval: 700
+    duration: 3000,
+    interval: 500
 });
 
 sr.reveal('.carousel-inner', {
     duration: 3000,
 
+});
+
+sr.reveal('.row-inferiorv2', {
+    duration: 3000,
+    interval: 500
 });
